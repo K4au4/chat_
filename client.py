5bd814a12e58c129d227d2b2f4edc2eb0351e94f
K4au4 <74125442+K4au4@users.noreply.github.com>
2020-11-08 14:54:07 +1000

    Add files via upload

--- client.py
+++ client.py
@@ -5,14 +5,14 @@ key = 8194
 shutdown = False
 join = False
 
-def receving (name, sock):
+def receving (name, sock):#Принимает данные от пользователей
 	while not shutdown:
 		try:
 			while True:
 				data, addr = sock.recvfrom(1024)
-				#print(data.decode("utf-8"))
+				
 
-				# Begin
+				
 				decrypt = ""; k = False
 				for i in data.decode("utf-8"):
 					if i == ":":
@@ -23,47 +23,48 @@ def receving (name, sock):
 					else:
 						decrypt += chr(ord(i)^key)
 				print(decrypt)
-				# End
+				
 
 				time.sleep(0.2)
 		except:
 			pass
 host = ''
-port = 0
+port = 0#Для подключчения клиента
 
-server = ("192.168.1.6",9090)
+server = ("192.168.1.6",8080)# указываем переменный сервер
 
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.bind((host,port))
-s.setblocking(0)
+s.bind((host,port))# для присвоения Имени клиенту
+s.setblocking(0)#чтоб не вылазила ошибка
 
-alias = input("Name: ")
+alias = input("Name: ")# ПРисвоение имени
 
-rT = threading.Thread(target = receving, args = ("RecvThread",s))
+rT = threading.Thread(target = receving, args = ("RecvThread",s))#Работа с многопоточностью (Функция (RecvThread", s) всегда обрабатывается с UPD)
 rT.start()
 
-while shutdown == False:
-	if join == False:
-		s.sendto(("["+alias + "] => join chat ").encode("utf-8"),server)
+while shutdown == False:# Пока клиент не вышел
+	if join == False:# ПОка нет пользователя
+		s.sendto(("["+alias + "] => join chat ").encode("utf-8"),server)#отправление сообщения от пользователя
 		join = True
 	else:
 		try:
-			message = input()
+			message = input()#Ввод сообщения и его обработка на сервере
 
-			# Begin
+			
 			crypt = ""
 			for i in message:
 				crypt += chr(ord(i)^key)
 			message = crypt
-			# End
+			
 
 			if message != "":
-				s.sendto(("["+alias + "] :: "+message).encode("utf-8"),server)
+				s.sendto(("["+alias + "] :: "+message).encode("utf-8"),server)# Если сообщение пустое, то его не защитывается со стороны сервера
+
 			
-			time.sleep(0.2)
+			time.sleep(0.2)#Диапозон между отправкой или получением сообщений
 		except:
 			s.sendto(("["+alias + "] <= left chat ").encode("utf-8"),server)
 			shutdown = True
 
-rT.join()
-s.close()
+rT.join()#функция может просматривать всю программу. Благодаря ей пользователи контактируют без разрывов во времени
+s.close()#Закрываем соединени
--- server.py
+++ server.py
@@ -1,33 +1,34 @@
 import socket, time
 
 host = ''
-port = 8080
+port = 8080# Во избежания блокировки подключения(если порт требует рута), или запуске чего-то на порте
 
-clients = []
+clients = []# список клиентов
 
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.bind((host,port))
 
-quit = False
-print("[ Server Started ]")
+quit = False #Во избежания бесконечного цикла
+print("[ Сервер запущен ]")
+print ("[Выполнено студентами группы 1141:\nСухотским Дмитрием и Патерикиной Полиной]")
 
 while not quit:
 	try:
-		data, addr = s.recvfrom(1024)
+		data, addr = s.recvfrom(1024)# 2 переменные (данные и адрес) для серва
 
-		if addr not in clients:
+		if addr not in clients:# Если у адреса нет клиента, то добавлеим клиента к адресу
 			clients.append(addr)
 
-		itsatime = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
+		itsatime = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())# Преобразование даты в переменную
 
-		print("["+addr[0]+"]=["+str(addr[1])+"]=["+itsatime+"]/",end="")
-		print(data.decode("utf-8"))
+		print("["+addr[0]+"]=["+str(addr[1])+"]=["+itsatime+"]/",end="")# Приобразование даты в переменную
+		print(data.decode("utf-8"))# Сообщение выводится в utf-8 после декодировки
 
-		for client in clients:
-			if addr != client:
+		for client in clients:# Смотрим клиентов
+			if addr != client:# Если адрес не равен клиенту, то сообщение отправляется некст клиенту (Чтобы клиент не получал свои сообщения)
 				s.sendto(data,client)
 	except:	
-		print("\n[ Server Stopped ]")
+		print("\n[ Сервер остановлен ]")
 		quit = True
 		
 s.close()
